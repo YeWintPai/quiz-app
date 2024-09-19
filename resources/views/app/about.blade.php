95c8d40c4baffbaecf178c34e930439b88d72a71
YeWintPai <artistrunorl8@gmail.com>
2024-09-20 01:21:10 +0630

    storage link

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="{{asset('/storage/app/css/about.style.css')}}">
-        <link rel="stylesheet" href="{{asset('storage/app/css/style.css')}}" />
+        <link rel="stylesheet" href="{{asset('./storage/app/css/style.css')}}" />
         <link rel="stylesheet" href="{{asset('storage/app/css/responsive.css')}}" />
 </head>
 
